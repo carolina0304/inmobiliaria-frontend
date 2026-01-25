940c4389086540c2e598c819db091a2206d72bac
carolina0304 <garlene122@gmail.com>
2026-01-25 12:52:54 -0600

    carrousel

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,24 @@
-import logo from "../../Images/LogoTerra.png";
+import logo from "../../Images/LogoTerra.jpeg";
+import logolinea from "../../Images/Line.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
       <div>
         <img className="header__logo" src={logo} alt="Logotipo TerraQro" />
+        <nav className="header__navigation">
+          <Link to="/" className="header__linkProp">
+            Propiedades
+          </Link>
+          <Link to="/contacto" className="header__linkCont">
+            Contacto
+          </Link>
+        </nav>
+        <img className="header__line" src={logolinea} alt="Linea" />
       </div>
     </header>
   );
 };
+
+export default Header;
