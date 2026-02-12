68fe04cce022802e181548182f4a7e938931d4bb
carolina0304 <garlene122@gmail.com>
2026-02-11 18:48:03 -0600

    Carousel

---
+++ src/components/Main/components/PropertyCard/PropertyCard.jsx
@@ -0,0 +1,13 @@
+function PropertyCard({ image, headline, description }) {
+  return (
+    <div className="property-card">
+      <img src={image} alt={headline} className="property-card__image" />
+      <div className="property-card__content">
+        <h3 className="property-card__headline">{headline}</h3>
+        <p className="property-card__description">{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default PropertyCard;
