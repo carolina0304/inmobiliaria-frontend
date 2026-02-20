933fb5566cc05f13747575e68a2fed25c9e0ad64
carolina0304 <garlene122@gmail.com>
2026-02-19 18:30:00 -0600

    css acomodar header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,18 +5,16 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="header">
-      <div>
-        <img className="header__logo" src={logo} alt="Logotipo TerraQro" />
-        <nav className="header__navigation">
-          <Link to="/" className="header__linkProp">
-            Propiedades
-          </Link>
-          <Link to="/contacto" className="header__linkCont">
-            Contacto
-          </Link>
-        </nav>
-        <img className="header__line" src={logolinea} alt="Linea" />
-      </div>
+      <img className="header__logo" src={logo} alt="Logotipo TerraQro" />
+      <nav className="header__navigation">
+        <Link to="/" className="header__linkProp">
+          Propiedades
+        </Link>
+        <Link to="/contacto" className="header__linkCont">
+          Contacto
+        </Link>
+      </nav>
+      <img className="header__line" src={logolinea} alt="Linea" />
     </header>
   );
 };
