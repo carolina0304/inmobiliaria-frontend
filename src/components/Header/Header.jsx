943de37b6de08461e4abffa485e7bbfcacffe4b2
carolina0304 <garlene122@gmail.com>
2026-02-20 16:42:15 -0600

    flexbox

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,20 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="header">
-      <img className="header__logo" src={logo} alt="Logotipo TerraQro" />
-      <nav className="header__navigation">
-        <Link to="/" className="header__linkProp">
-          Propiedades
-        </Link>
-        <Link to="/contacto" className="header__linkCont">
-          Contacto
-        </Link>
-      </nav>
+    <div>
+      <header className="header">
+        <img className="header__logo" src={logo} alt="Logotipo TerraQro" />
+        <nav className="header__navigation">
+          <Link to="/" className="header__linkProp">
+            Propiedades
+          </Link>
+          <Link to="/contacto" className="header__linkCont">
+            Contacto
+          </Link>
+        </nav>
+      </header>
       <img className="header__line" src={logolinea} alt="Linea" />
-    </header>
+    </div>
   );
 };
 
